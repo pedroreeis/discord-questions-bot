8e1a3b8bb372b3e2ea9659e2825fdf247275d639
Pedro Reis <61993076+queendeveloperbr@users.noreply.github.com>
2021-08-14 16:15:45 -0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,12 +1,11 @@
 /* Para usufruir do bot, você precisa de um banco de dados com essas tabelas. */
 
 CREATE TABLE `zc_questions` (
-	`question_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`question_id` INT(4) NOT NULL AUTO_INCREMENT,
 	`question_name` TEXT NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
 	`question_keywords` TEXT NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
 	`question_answers` TEXT NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
-	PRIMARY KEY (`question_id`) USING BTREE,
-	INDEX `question_id` (`question_id`) USING BTREE
+	PRIMARY KEY (`question_id`) USING BTREE
 )
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
